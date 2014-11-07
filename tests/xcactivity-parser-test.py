06840a07f96c5479cd3b1e81992ce8631b2587cc
Paul Taykalo <tt.kilew@gmail.com>
2014-11-08 01:01:24 +0200

    usin six to determine which python version is running

--- tests/xcactivity-parser-test.py
+++ tests/xcactivity-parser-test.py
@@ -1,6 +1,14 @@
 import subprocess
+import six
 
 script_path='./src/xcactivity-parser.py'
+
+def string_from_std(std):
+	if six.PY2:
+		return std
+	return str( std, encoding='utf8' )
+
+
 
 def test_script_run():
 	sp = subprocess.Popen([script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -11,43 +19,43 @@ def test_script_run():
 def test_usage_message_on_no_params():
 	sp = subprocess.Popen([script_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = sp.communicate()
-	assert "Usage:" in err
+	assert "Usage:" in string_from_std(err)
 
 def test_usage_message_on_only_file_parameter():
 	sp = subprocess.Popen([script_path, "-x", "/"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = sp.communicate()
-	assert "Usage:" in err
+	assert "Usage:" in string_from_std(err)
 
 def test_usage_message_on_only_directory_parameter():
 	sp = subprocess.Popen([script_path, "-f", "/"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = sp.communicate()
-	assert "Usage:" in err
+	assert "Usage:" in string_from_std(err)
 
 def test_usage_message_on_only_arch_parameter():
 	sp = subprocess.Popen([script_path, "-a", "i386"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = sp.communicate()
-	assert "Usage:" in err	
+	assert "Usage:" in string_from_std(err)	
 
 def test_searching_message_on_two_parameters():
 	sp = subprocess.Popen([script_path, "-f", "/", "-x", "/"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = sp.communicate()
-	assert "Usage:" in err
+	assert "Usage:" in string_from_std(err)
 
 def test_searching_message_on_all_parameters():
 	sp = subprocess.Popen([script_path, "-f", "/", "-x", "/", "-a", "i386"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = sp.communicate()
-	assert not "Usage:" in err
+	assert not "Usage:" in string_from_std(err)
 
 def test_full_path_for_file():
 	sp = subprocess.Popen([script_path, "-f", "asd", "-x", "/"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = sp.communicate()
-	assert "full path" in err
+	assert "full path" in string_from_std(err)
 
 def test_directory_existance():
 	sp = subprocess.Popen([script_path, "-f", "/", "-x", "asfsd"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = sp.communicate()
-	assert "directory" in err
-	assert "exist" in err
+	assert "directory" in string_from_std(err)
+	assert "exist" in string_from_std(err)
 
 #  Working with real files :)
 
@@ -59,10 +67,10 @@ def test_arguments_on_existed_file():
 	compilationString="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang -x objective-c -arch i386 -fmessage-length=0 -fdiagnostics-show-note-include-stack -fmacro-backtrace-limit=0 -std=gnu99 -fobjc-arc -fmodules -fmodules-cache-path=/Users/ptaykalo/Library/Developer/Xcode/DerivedData/ModuleCache -fmodules-prune-interval=86400 -fmodules-prune-after=345600 -Wnon-modular-include-in-framework-module -Werror=non-modular-include-in-framework-module -Wno-trigraphs -fpascal-strings -Os -Wno-missing-field-initializers -Wno-missing-prototypes -Werror=return-type -Wunreachable-code -Wno-implicit-atomic-properties -Werror=deprecated-objc-isa-usage -Werror=objc-root-class -Wno-receiver-is-weak -Wno-arc-repeated-use-of-weak -Wduplicate-method-match -Wno-missing-braces -Wparentheses -Wswitch -Wunused-function -Wno-unused-label -Wno-unused-parameter -Wunused-variable -Wunused-value -Wempty-body -Wconditional-uninitialized -Wno-unknown-pragmas -Wno-shadow -Wno-four-char-constants -Wno-conversion -Wconstant-conversion -Wint-conversion -Wbool-conversion -Wenum-conversion -Wshorten-64-to-32 -Wpointer-sign -Wno-newline-eof -Wno-selector -Wno-strict-selector-match -Wundeclared-selector -Wno-deprecated-implementations -DNS_BLOCK_ASSERTIONS=1 -DOBJC_OLD_DISPATCH_PROTOTYPES=0 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator8.1.sdk -fexceptions -fasm-blocks -fstrict-aliasing -Wprotocol -Wdeprecated-declarations -g -Wno-sign-conversion -fobjc-abi-version=2 -fobjc-legacy-dispatch -mios-simulator-version-min=8.1 -iquote /Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Intermediates/dyci-framework.build/Release-iphonesimulator/dyci.build/dyci-generated-files.hmap -I/Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Intermediates/dyci-framework.build/Release-iphonesimulator/dyci.build/dyci-own-target-headers.hmap -I/Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Intermediates/dyci-framework.build/Release-iphonesimulator/dyci.build/dyci-all-target-headers.hmap -iquote /Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Intermediates/dyci-framework.build/Release-iphonesimulator/dyci.build/dyci-project-headers.hmap -I/Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Products/Release-iphonesimulator/include -I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include -I/Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Intermediates/dyci-framework.build/Release-iphonesimulator/dyci.build/DerivedSources/i386 -I/Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Intermediates/dyci-framework.build/Release-iphonesimulator/dyci.build/DerivedSources -F/Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Products/Release-iphonesimulator -MMD -MT dependencies -MF /Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Intermediates/dyci-framework.build/Release-iphonesimulator/dyci.build/Objects-normal/i386/SFInjectionsNotificationsCenter.d --serialize-diagnostics /Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Intermediates/dyci-framework.build/Release-iphonesimulator/dyci.build/Objects-normal/i386/SFInjectionsNotificationsCenter.dia -c /Volumes/data/Projects/dyci-main/Dynamic\ Code\ Injection/dyci/Classes/Notifications/SFInjectionsNotificationsCenter.m -o /Users/ptaykalo/Library/Developer/Xcode/DerivedData/DYCI-hfgqlcmijbqsjtcnjyhbxgpcckwj/Build/Intermediates/dyci-framework.build/Release-iphonesimulator/dyci.build/Objects-normal/i386/SFInjectionsNotificationsCenter.o"
 	sp = subprocess.Popen([script_path, "-f", existingfile, "-x", fixtures_path, "-a", "i386"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	out, err = sp.communicate()
-	assert not "Usage:" in err
+	assert not "Usage:" in string_from_std(err)
 	# print out
 	# print err
-	assert compilationString == out.strip()
+	assert compilationString == string_from_std(out).strip()
 
 def test_arguments_on_existed_file():
 	existingfile="/Volumes/data/Projects/dyci-main/Dynamic\ Code\ Injection/dyci/Classes/Notifications/SFInjectionsNotificationsCenter.m"
@@ -72,7 +80,7 @@ def test_arguments_on_existed_file():
 	assert not "Usage:" in err
 	# print out
 	# print err
-	assert compilationString == out.strip() 	
+	assert compilationString == string_from_std(out).strip() 	
 
 def test_arguments_on_existed_file_different_architecture():
 	existingfile="/Volumes/data/Projects/dyci-main/Dynamic\ Code\ Injection/dyci/Classes/Notifications/SFInjectionsNotificationsCenter.m"
@@ -82,5 +90,5 @@ def test_arguments_on_existed_file_different_architecture():
 	assert not "Usage:" in err
 	# print out
 	# print err
-	assert compilationString != out.strip()
+	assert compilationString != string_from_std(out).strip()
 
