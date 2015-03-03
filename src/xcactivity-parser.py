c4f9607bf7cedc5aed38de99a4d74d7854b62573
Paul Taykalo <tt.kilew@gmail.com>
2015-03-03 19:02:44 +0200

    Added support for working directory -w flag will add working directory to output (if any found)

--- src/xcactivity-parser.py
+++ src/xcactivity-parser.py
@@ -10,6 +10,7 @@ def main():
     p.add_option('--xcactivity-files-dir', '-x', help="Directory where .xcactivity files are located")
     p.add_option('--file', '-f', help="File for which parameters are searching for")
     p.add_option('--arch', '-a', help="Architecture for which file was compiled")
+    p.add_option("--working-dir", "-w", action="store_true", dest="show_working_dir", default=False, help="Add working dir before compilation line")
 
     options, arguments = p.parse_args()
     if not options.xcactivity_files_dir:
@@ -29,28 +30,33 @@ def main():
     if not options.arch:
         p.error("Please specify architecture")
 
-    search_copilation_string_for_file_in_directory(options.file, options.xcactivity_files_dir, options.arch)
+    search_copilation_string_for_file_in_directory(options.file, options.xcactivity_files_dir, options.arch, options.show_working_dir)
 
 
-def search_file_in_activity_log(searching_file, activity_log_file, architecture):
+def search_file_in_activity_log(searching_file, activity_log_file, architecture, show_working_dir = False):
     f = gzip.open(activity_log_file, 'rUb')
+    latest_working_dir = None
     for line in f.readlines():
         if searching_file in line and "/XcodeDefault.xctoolchain" in line:
             for rline in line.split("\r"):
+                if rline.startswith("cd "):
+                    latest_working_dir = rline[3:].strip()
                 if searching_file in rline and "/XcodeDefault.xctoolchain" in rline and "-arch " + architecture in rline:
                     if rline.endswith(".o"):
+                        if show_working_dir == True:
+                            yield latest_working_dir
                         yield rline.strip()
 
     f.close()
 
 
-def search_copilation_string_for_file_in_directory(searching_file, directory, architecture):
+def search_copilation_string_for_file_in_directory(searching_file, directory, architecture, show_working_dir = False):
     # out, err = subprocess.Popen(["ls","-t", directory + "/*.xcactivitylog"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     out, err = subprocess.Popen(["ls", "-t", directory], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     for currentfile in out.splitlines():
         if currentfile.endswith(".xcactivitylog"):
             for compilation_params in search_file_in_activity_log(searching_file, os.path.join(directory, currentfile),
-                                                                  architecture):
+                                                                  architecture, show_working_dir):
                 print(compilation_params)
 
 
